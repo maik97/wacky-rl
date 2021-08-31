b9573a5eaf212e5ac7685d16540ccb5391b369f2
maik97 <35041716+maik97@users.noreply.github.com>
2021-08-31 20:20:14 +0200

    update

--- wacky_rl/agents/_agent_core.py
+++ wacky_rl/agents/_agent_core.py
@@ -1,9 +1,10 @@
+import numpy as np
 from wacky_rl import layers
 
 class AgentCore:
 
     def __init__(self):
-        pass
+        self.approx_contin = False
 
     def __call__(self, *args, **kwargs):
         return self.act( *args, **kwargs)
@@ -46,6 +47,16 @@ class AgentCore:
         if self.space_is_discrete(env.action_space):
             return layers.DiscreteActionLayer(num_bins=num_actions)
         elif approx_contin:
+            self.approx_contin = True
             return layers.DiscreteActionLayer(num_bins=num_bins, num_actions=num_actions)
         else:
             return layers.ContinActionLayer(num_actions=num_actions)
+
+
+    def transform_actions(self, dist, actions, lows=-1.0, highs=1.0, scale=1.0):
+
+        if self.approx_contin:
+            actions = dist.discrete_to_contin(actions)
+
+        actions = np.squeeze(actions.numpy()) * scale
+        return np.clip(actions, a_min=lows, a_max=highs)
